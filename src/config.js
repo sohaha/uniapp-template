a656d62cb501ab0cb7dd13bf0ec6f0e02afa1bf4
seekwe <seekwe@gmail.com>
2020-09-21 17:32:03 +0800

    chore: 🐳 更新依赖

--- src/config.js
+++ src/config.js
@@ -12,8 +12,8 @@ if (process.env.NODE_ENV !== 'development') {
   apiHost = 'http://weapp.test'; // 接口域名
   websiteUrl = 'http://weapp.test'; // 静态资源域名
 } else {
-  apiHost = 'http://127.0.0.1:3780'; // 测试接口域名
-  websiteUrl = 'http://127.0.0.1:3780'; // 测试静态资源域名
+  apiHost = 'http://127.0.0.1:3783'; // 测试接口域名
+  websiteUrl = 'http://127.0.0.1:3783'; // 测试静态资源域名
 }
 
 export default {
